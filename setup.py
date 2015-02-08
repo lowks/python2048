740923f1973e682255b324108248bc9d2aa00aa4
lowks <kianseong@gmail.com>
2015-02-08 15:32:56 +0800

    Re-org and adding setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='python2048',
+      description='Implementation of the 2048 game',
+      author='lowks',
+      author_email='kianseong@gmail.com',
+      test_suite='nose.collector',
+      url='https://github.com/lowks/python2048',)
